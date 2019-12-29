dea32449bdb98f27bb04c787ae6503651abcd762
abdessalam-aadel <abdessalam.aadel@gmail.com>
2019-12-29 01:33:07 +0100

    Update form of Conversion_Multimedia

--- Conversion_Multimedia/Form1.Designer.cs
+++ Conversion_Multimedia/Form1.Designer.cs
@@ -28,78 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox = new System.Windows.Forms.ComboBox();
-            this.progressBar1 = new Conversion_Multimedia.ProgressBar();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConversionMultimediaForm));
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnLetsGo = new System.Windows.Forms.Button();
+            this.labelOfTypes = new System.Windows.Forms.Label();
             this.choseFile1 = new Conversion_Multimedia.ChoseFile();
-            this.startConversion = new Conversion_Multimedia.StartConversion();
+            this.startConversion1 = new Conversion_Multimedia.StartConversion();
             this.SuspendLayout();
             // 
-            // comboBox
+            // btnBack
             // 
-            this.comboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.comboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
-            this.comboBox.FormattingEnabled = true;
-            this.comboBox.Items.AddRange(new object[] {
-            "Extract sound from video",
-            ".wav to .mp3",
-            ".avi to .mpg",
-            ".mpg to .avi",
-            ".avi to .flv",
-            ".avi to .gif",
-            ".avi to .dv",
-            ".avi to .mpeg",
-            ".avi to .mp4"});
-            this.comboBox.Location = new System.Drawing.Point(135, 196);
-            this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(178, 21);
-            this.comboBox.TabIndex = 4;
-            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            this.btnBack.BackColor = System.Drawing.SystemColors.Control;
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Location = new System.Drawing.Point(26, 142);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(58, 21);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Visible = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // progressBar1
+            // btnLetsGo
             // 
-            this.progressBar1.Location = new System.Drawing.Point(14, 6);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(420, 183);
-            this.progressBar1.TabIndex = 7;
+            this.btnLetsGo.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLetsGo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLetsGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLetsGo.Location = new System.Drawing.Point(171, 141);
+            this.btnLetsGo.Name = "btnLetsGo";
+            this.btnLetsGo.Size = new System.Drawing.Size(103, 21);
+            this.btnLetsGo.TabIndex = 7;
+            this.btnLetsGo.Text = "Let\'s Go";
+            this.btnLetsGo.UseVisualStyleBackColor = false;
+            this.btnLetsGo.Click += new System.EventHandler(this.btnLetsGo_Click);
+            // 
+            // labelOfTypes
+            // 
+            this.labelOfTypes.AutoSize = true;
+            this.labelOfTypes.ForeColor = System.Drawing.Color.ForestGreen;
+            this.labelOfTypes.Location = new System.Drawing.Point(320, 145);
+            this.labelOfTypes.Name = "labelOfTypes";
+            this.labelOfTypes.Size = new System.Drawing.Size(16, 13);
+            this.labelOfTypes.TabIndex = 8;
+            this.labelOfTypes.Text = "...";
+            this.labelOfTypes.Visible = false;
             // 
             // choseFile1
             // 
-            this.choseFile1.Location = new System.Drawing.Point(14, 6);
+            this.choseFile1.Location = new System.Drawing.Point(12, 11);
             this.choseFile1.Name = "choseFile1";
-            this.choseFile1.Size = new System.Drawing.Size(420, 183);
-            this.choseFile1.TabIndex = 6;
+            this.choseFile1.Size = new System.Drawing.Size(420, 124);
+            this.choseFile1.TabIndex = 10;
             // 
-            // startConversion
+            // startConversion1
             // 
-            this.startConversion.Location = new System.Drawing.Point(14, 7);
-            this.startConversion.Name = "startConversion";
-            this.startConversion.Size = new System.Drawing.Size(420, 183);
-            this.startConversion.TabIndex = 5;
+            this.startConversion1.Location = new System.Drawing.Point(12, 11);
+            this.startConversion1.Name = "startConversion1";
+            this.startConversion1.Size = new System.Drawing.Size(420, 124);
+            this.startConversion1.TabIndex = 9;
             // 
             // ConversionMultimediaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 230);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(444, 172);
             this.Controls.Add(this.choseFile1);
-            this.Controls.Add(this.startConversion);
-            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.startConversion1);
+            this.Controls.Add(this.labelOfTypes);
+            this.Controls.Add(this.btnLetsGo);
+            this.Controls.Add(this.btnBack);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "ConversionMultimediaForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Conversion Multimedia";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ComboBox comboBox;
-        private StartConversion startConversion;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnLetsGo;
+        private System.Windows.Forms.Label labelOfTypes;
+        private StartConversion startConversion1;
         private ChoseFile choseFile1;
-        private ProgressBar progressBar1;
     }
 }
 
