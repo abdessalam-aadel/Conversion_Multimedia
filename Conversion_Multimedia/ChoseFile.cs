41028ce94af0d7ca48f52d458813e7634b1047ae
abdessalam-aadel <abdessalam.aadel@gmail.com>
2019-12-29 01:21:32 +0100

    Update : passing value of type convertion from Form to userControl (ChoseFile.cs) and looking for

--- Conversion_Multimedia/ChoseFile.cs
+++ Conversion_Multimedia/ChoseFile.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 
@@ -13,21 +8,106 @@ namespace Conversion_Multimedia
 {
     public partial class ChoseFile : UserControl
     {
+        public string Types;
+        public string TypesOutput;
+        public string CommandFFmpegMiddle;
 
         public ChoseFile()
         {
             InitializeComponent();
         }
 
+        // passing value of type convertion from Form to userControl (ChoseFile.cs)
+        public string PassingTypeOfConversion
+        {
+            set { Types = value; }
+        }
+
         private void btnLoad_Click(object sender, EventArgs e)
         {
             // Open file dialog and customize
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Videos files|*.mp4";
-            ofd.DefaultExt = "mp4";
             ofd.InitialDirectory = "C:/";
             ofd.RestoreDirectory = true;
             ofd.Title = "Chose your file";
+
+            #region Locking fo your type ...
+            if (Types == "Extract sound from video")
+            {
+                ofd.Filter = "Videos files|*.mp4";
+                ofd.DefaultExt = "mp4";
+                TypesOutput = ".mp3";
+                CommandFFmpegMiddle = " -vn -ar 44100 -ac 2 -ab 192k -f mp3 ";
+            }
+
+            else if (Types == ".wav to .mp3")
+            {
+                ofd.Filter = "Videos files|*.wav";
+                ofd.DefaultExt = "wav";
+                TypesOutput = ".mp3";
+                CommandFFmpegMiddle = " -vn -ar 44100 -ac 2 -ab 192k -f mp3 ";
+            }
+
+            else if (Types == ".avi to .mpg")
+            {
+                ofd.Filter = "Videos files|*.avi";
+                ofd.DefaultExt = "avi";
+                TypesOutput = ".mpg";
+                CommandFFmpegMiddle = " ";
+            }
+
+            else if (Types == ".mpg to .avi")
+            {
+                ofd.Filter = "Videos files|*.mpg";
+                ofd.DefaultExt = "mpg";
+                TypesOutput = ".avi";
+                CommandFFmpegMiddle = " ";
+            }
+
+            else if (Types == ".avi to .flv")
+            {
+                ofd.Filter = "Videos files|*.avi";
+                ofd.DefaultExt = "avi";
+                TypesOutput = ".flv";
+                CommandFFmpegMiddle = " -ab 56 -ar 44100 -b 200 -r 15 -s 320x240 -f flv ";
+            }
+
+            else if (Types == ".avi to .gif")
+            {
+                ofd.Filter = "Videos files|*.avi";
+                ofd.DefaultExt = "avi";
+                TypesOutput = ".gif";
+                CommandFFmpegMiddle = " ";
+            }
+
+            else if (Types == ".avi to .dv")
+            {
+                ofd.Filter = "Videos files|*.avi";
+                ofd.DefaultExt = "avi";
+                TypesOutput = ".dv";
+                CommandFFmpegMiddle = " -s pal -r pal -aspect 4:3 -ar 48000 -ac 2 ";
+            }
+
+            else if (Types == ".avi to .mpeg")
+            {
+                ofd.Filter = "Videos files|*.avi";
+                ofd.DefaultExt = "avi";
+                TypesOutput = ".mpeg";
+                CommandFFmpegMiddle = " -target pal-dvd -ps 2000000000 -aspect 16:9 ";
+            }
+
+            else if (Types == ".avi to .mp4")
+            {
+                ofd.Filter = "Videos files|*.avi";
+                ofd.DefaultExt = "avi";
+                TypesOutput = ".mp4";
+                CommandFFmpegMiddle = " ";
+            }
+
+            else
+                MessageBox.Show("This méthode could not exist !");
+            #endregion
+
             DialogResult result = ofd.ShowDialog();
             if (result == DialogResult.OK)
             {
@@ -41,7 +121,7 @@ namespace Conversion_Multimedia
         {
             try
             {
-                // change the cursor and disable butto start
+                // change the cursor and disable button start
                 this.Cursor = Cursors.WaitCursor;
                 btnStart.Enabled = false;
 
@@ -54,7 +134,7 @@ namespace Conversion_Multimedia
                     // Given that is started without a window
                     process.StartInfo.CreateNoWindow = true;
 
-                    //use the redirected input -output
+                    // uses the redirected input-output
                     process.StartInfo.RedirectStandardInput = true;
                     process.StartInfo.RedirectStandardOutput = true;
 
@@ -63,7 +143,7 @@ namespace Conversion_Multimedia
 
                     // Declare variable input and output of FFmpeg tools
                     string input = txtboxFileName.Text;
-                    string output = "output_" + labelFilename.Text.Replace(".mp4", "") + ".avi";
+                    string output = "output_" + labelFilename.Text.Replace(TypesOutput, "") + TypesOutput;
                     string ffmpeg;
 
                     // Start Condition : if you have win32 or win64
@@ -76,7 +156,7 @@ namespace Conversion_Multimedia
                     //process.StandardInput.WriteLine("wmic os get osarchitecture");
 
                     // Start Command line ...
-                    process.StandardInput.WriteLine(ffmpeg + " -i " + input + " " + output);
+                    process.StandardInput.WriteLine(ffmpeg + " -i " + input + CommandFFmpegMiddle + output);
                     
                     // this command just for test
                     //process.StandardInput.WriteLine("ipconfig");
@@ -123,7 +203,10 @@ namespace Conversion_Multimedia
             }
             catch (Exception ex)
             {
-                MessageBox.Show("The following exception was raised: \n" + ex.Message);
+                MessageBox.Show("The following exception was raised: \n" + ex.Message,
+                            "Error Message",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
                 ChangeToDefault();
             }
         }
