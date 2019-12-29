8f1284a0b1b8be7d9d65f0b1b4ceb172f406348d
abdessalam-aadel <abdessalam.aadel@gmail.com>
2019-12-29 01:28:54 +0100

    Update : StartConversion.Designer.cs

--- Conversion_Multimedia/StartConversion.Designer.cs
+++ Conversion_Multimedia/StartConversion.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.ComboBoxTypes = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -36,19 +38,43 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(141, 67);
+            this.label1.Location = new System.Drawing.Point(141, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 48);
+            this.label1.Size = new System.Drawing.Size(138, 16);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Start your Conversion\r\n\r\nChose your methode :";
+            this.label1.Text = "Chose your methode :";
+            // 
+            // ComboBoxTypes
+            // 
+            this.ComboBoxTypes.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.ComboBoxTypes.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.ComboBoxTypes.Location = new System.Drawing.Point(116, 93);
+            this.ComboBoxTypes.MaxDropDownItems = 10;
+            this.ComboBoxTypes.Name = "ComboBoxTypes";
+            this.ComboBoxTypes.Size = new System.Drawing.Size(188, 21);
+            this.ComboBoxTypes.TabIndex = 7;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label11.Location = new System.Drawing.Point(130, 26);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(161, 20);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Start your Conversion";
             // 
             // StartConversion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.ComboBoxTypes);
             this.Controls.Add(this.label1);
             this.Name = "StartConversion";
-            this.Size = new System.Drawing.Size(420, 183);
+            this.Size = new System.Drawing.Size(420, 124);
+            this.Load += new System.EventHandler(this.StartConversion_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +82,7 @@
 
         #endregion
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ComboBoxTypes;
+        private System.Windows.Forms.Label label11;
     }
 }
