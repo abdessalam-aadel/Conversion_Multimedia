ac6937cdd996694c28d92cd1edce77168e24ce4e
abdessalam-aadel <abdessalam.aadel@gmail.com>
2020-01-10 19:44:06 +0000

    Add new form (FrmInfo.cs): Get information for video file

---
+++ Conversion_Multimedia/FrmInfo.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Conversion_Multimedia
+{
+    partial class FrmInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtBox = new System.Windows.Forms.RichTextBox();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxtBox
+            // 
+            this.rtxtBox.BackColor = System.Drawing.Color.Silver;
+            this.rtxtBox.Location = new System.Drawing.Point(12, 12);
+            this.rtxtBox.Name = "rtxtBox";
+            this.rtxtBox.Size = new System.Drawing.Size(461, 286);
+            this.rtxtBox.TabIndex = 0;
+            this.rtxtBox.Text = "";
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.OrangeRed;
+            this.BtnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnExit.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.ForeColor = System.Drawing.Color.White;
+            this.BtnExit.Location = new System.Drawing.Point(147, 307);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(191, 23);
+            this.BtnExit.TabIndex = 1;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = false;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // FrmInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 338);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.rtxtBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get Information for your video file";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtBox;
+        private System.Windows.Forms.Button BtnExit;
+    }
+}
---
+++ Conversion_Multimedia/FrmInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Conversion_Multimedia
+{
+    public partial class FrmInfo : Form
+    {
+        public FrmInfo() => InitializeComponent();
+
+        public void GetValue(string info) => rtxtBox.Text += info;
+
+        // Handle event click for Button Exit ..
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            rtxtBox.Clear();
+            this.Close();
+        }
+    }
+}
