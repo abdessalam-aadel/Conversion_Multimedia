790b68f258e3d3459ac02f9ac5f7da6bef7d89f0
abdessalam-aadel <abdessalam.aadel@gmail.com>
2020-01-10 19:41:42 +0000

    Add new user control (Crop.cs): Crop a video file from the top left corner

---
+++ Conversion_Multimedia/Crop.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Conversion_Multimedia
+{
+    partial class Crop
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxH = new System.Windows.Forms.TextBox();
+            this.txtBoxW = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxVideoFilename = new System.Windows.Forms.TextBox();
+            this.BtnLoadVideo = new System.Windows.Forms.Button();
+            this.btnStartCrop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxX
+            // 
+            this.txtBoxX.Enabled = false;
+            this.txtBoxX.Location = new System.Drawing.Point(39, 72);
+            this.txtBoxX.Name = "txtBoxX";
+            this.txtBoxX.Size = new System.Drawing.Size(114, 20);
+            this.txtBoxX.TabIndex = 0;
+            this.txtBoxX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxX_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Y :";
+            // 
+            // txtBoxY
+            // 
+            this.txtBoxY.Enabled = false;
+            this.txtBoxY.Location = new System.Drawing.Point(39, 101);
+            this.txtBoxY.Name = "txtBoxY";
+            this.txtBoxY.Size = new System.Drawing.Size(114, 20);
+            this.txtBoxY.TabIndex = 3;
+            this.txtBoxY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxY_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Width :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Height :";
+            // 
+            // txtBoxH
+            // 
+            this.txtBoxH.Enabled = false;
+            this.txtBoxH.Location = new System.Drawing.Point(212, 100);
+            this.txtBoxH.Name = "txtBoxH";
+            this.txtBoxH.Size = new System.Drawing.Size(114, 20);
+            this.txtBoxH.TabIndex = 7;
+            this.txtBoxH.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxH_KeyPress);
+            // 
+            // txtBoxW
+            // 
+            this.txtBoxW.Enabled = false;
+            this.txtBoxW.Location = new System.Drawing.Point(212, 71);
+            this.txtBoxW.Name = "txtBoxW";
+            this.txtBoxW.Size = new System.Drawing.Size(114, 20);
+            this.txtBoxW.TabIndex = 6;
+            this.txtBoxW.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxW_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(98, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(224, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Crop from the top left corner";
+            // 
+            // txtBoxVideoFilename
+            // 
+            this.txtBoxVideoFilename.Location = new System.Drawing.Point(16, 41);
+            this.txtBoxVideoFilename.Name = "txtBoxVideoFilename";
+            this.txtBoxVideoFilename.ReadOnly = true;
+            this.txtBoxVideoFilename.Size = new System.Drawing.Size(310, 20);
+            this.txtBoxVideoFilename.TabIndex = 23;
+            this.txtBoxVideoFilename.Text = "Chose your video file ...";
+            // 
+            // BtnLoadVideo
+            // 
+            this.BtnLoadVideo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLoadVideo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLoadVideo.Location = new System.Drawing.Point(332, 40);
+            this.BtnLoadVideo.Name = "BtnLoadVideo";
+            this.BtnLoadVideo.Size = new System.Drawing.Size(75, 22);
+            this.BtnLoadVideo.TabIndex = 22;
+            this.BtnLoadVideo.Text = "...";
+            this.BtnLoadVideo.UseVisualStyleBackColor = true;
+            this.BtnLoadVideo.Click += new System.EventHandler(this.BtnLoadVideo_Click);
+            // 
+            // btnStartCrop
+            // 
+            this.btnStartCrop.BackColor = System.Drawing.Color.Indigo;
+            this.btnStartCrop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStartCrop.Enabled = false;
+            this.btnStartCrop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartCrop.ForeColor = System.Drawing.Color.White;
+            this.btnStartCrop.Location = new System.Drawing.Point(332, 71);
+            this.btnStartCrop.Name = "btnStartCrop";
+            this.btnStartCrop.Size = new System.Drawing.Size(75, 49);
+            this.btnStartCrop.TabIndex = 24;
+            this.btnStartCrop.Text = "Start";
+            this.btnStartCrop.UseVisualStyleBackColor = false;
+            this.btnStartCrop.Click += new System.EventHandler(this.btnStartCrop_Click);
+            // 
+            // Crop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnStartCrop);
+            this.Controls.Add(this.txtBoxVideoFilename);
+            this.Controls.Add(this.BtnLoadVideo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBoxH);
+            this.Controls.Add(this.txtBoxW);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxX);
+            this.Name = "Crop";
+            this.Size = new System.Drawing.Size(420, 124);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxH;
+        private System.Windows.Forms.TextBox txtBoxW;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxVideoFilename;
+        private System.Windows.Forms.Button BtnLoadVideo;
+        private System.Windows.Forms.Button btnStartCrop;
+    }
+}
---
+++ Conversion_Multimedia/Crop.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Conversion_Multimedia
+{
+    public partial class Crop : UserControl
+    {
+        public Crop() => InitializeComponent();
+
+        public string videoName, videoType;
+
+        // Customize Open file dialog 
+        OpenFileDialog ofd = new OpenFileDialog
+        {
+            InitialDirectory = "C:/",
+            RestoreDirectory = true,
+            Title = "Chose your file"
+        };
+
+        // Start methode : Not Enter a Key String just a key number...
+        private static void Not_KeyString(KeyPressEventArgs e)
+        {
+            if (e.KeyChar < 48 || e.KeyChar > 57)
+                e.Handled = true;
+        }
+        // Start -- Handle event KeyPress ...
+        private void txtBoxX_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+        private void txtBoxY_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+        private void txtBoxW_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+        private void txtBoxH_KeyPress(object sender, KeyPressEventArgs e) => Not_KeyString(e);
+        // End -- Event KeyPress ...
+
+        private void BtnLoadVideo_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Videos files (*.mp4, *.mov, *.m4a, *.3gp, *.3g2, *.mj2, *.avi, *.flv, *.wav) | *.mp4; *.mov; *.m4a; *.3gp; *.3g2; *.mj2; *.avi; *.flv; *.wav";
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtBoxVideoFilename.Text = ofd.FileName;
+                videoName = Path.GetFileNameWithoutExtension(ofd.SafeFileName); // Get File name without extension
+                videoType = Path.GetExtension(ofd.SafeFileName); // Get File Extension
+                EnabledBtnAndTxt();
+            }
+        }
+        // Methode Enabled button and textbox
+        public void EnabledBtnAndTxt()
+        {
+            btnStartCrop.Enabled = true;
+            txtBoxW.Enabled = true;
+            txtBoxH.Enabled = true;
+            txtBoxX.Enabled = true;
+            txtBoxY.Enabled = true;
+        }
+
+        // Handle event click Button Start Crop ...
+        private void btnStartCrop_Click(object sender, EventArgs e)
+        {
+            if (txtBoxW.Text != "" && txtBoxH.Text != "" && txtBoxX.Text != "" && txtBoxY.Text != "")
+            {
+                try
+                {
+                    // change the cursor and disable button start
+                    this.Cursor = Cursors.WaitCursor;
+                    BtnLoadVideo.Enabled = false;
+                    btnStartCrop.Enabled = false;
+                    // uses an instance of the Process class to start a process
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo.UseShellExecute = false;
+                        // run the cmd process
+                        process.StartInfo.FileName = "cmd.exe";
+                        // Given that is started without a window
+                        process.StartInfo.CreateNoWindow = true;
+
+                        // uses the redirected input-output
+                        process.StartInfo.RedirectStandardInput = true;
+                        process.StartInfo.RedirectStandardOutput = true;
+
+                        // Start process
+                        process.Start();
+
+                        // Declare variable input and output of FFmpeg tools
+                        string inputVideo = txtBoxVideoFilename.Text;
+
+                        string output = " output_" + videoName.Replace(" ", "_")
+                                        + "_Croped"
+                                        + videoType;
+                        string ffmpeg;
+
+                        // Start Condition : if you have win32 or win64
+                        if (Environment.Is64BitOperatingSystem)
+                            ffmpeg = @"tools\x64\bin\ffmpeg.exe"; // path of FFmpeg tools for win32
+                        else
+                            ffmpeg = @"tools\x32\bin\ffmpeg.exe"; // path of FFmpeg tools for win64
+
+                        // Start Command line ...
+                        process.StandardInput.WriteLine(ffmpeg + " -i " + "\"" + inputVideo + "\""
+                            + " -filter:v " + "\"" + "crop = " 
+                            + txtBoxW.Text + ":" + txtBoxH.Text + ":" + txtBoxX.Text + ":" + txtBoxY.Text + "\""
+                            + " -c:a copy" + output);
+
+                        // Flush & Close StandarInput
+                        process.StandardInput.Flush();
+                        process.StandardInput.Close();
+
+                        // Wait for Exit
+                        process.WaitForExit();
+
+                        // Close The Process
+                        process.Close();
+                    }
+                    ChangeToDefault();
+                    MessageBox.Show("Your video have been croped successfully", "Success",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ChangeToDefault();
+                }
+            }
+            else
+                MessageBox.Show("Please enter your video size ... \n\t(width & height)\nAnd enter starting position ...\n\t(x , y)");
+        }
+
+        // Methode Change to default
+        private void ChangeToDefault()
+        {
+            this.Cursor = DefaultCursor;
+            txtBoxVideoFilename.Text = "Chose your video file ...";
+            BtnLoadVideo.Enabled = true;
+            btnStartCrop.Enabled = false;
+            txtBoxW.Enabled = false;
+            txtBoxH.Enabled = false;
+            txtBoxX.Enabled = false;
+            txtBoxY.Enabled = false;
+            txtBoxW.Text = "";
+            txtBoxH.Text = "";
+            txtBoxX.Text = "";
+            txtBoxY.Text = "";
+            ofd.FileName = "";
+        }
+    }
+}
