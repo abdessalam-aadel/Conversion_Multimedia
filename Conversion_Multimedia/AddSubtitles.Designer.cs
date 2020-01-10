ce5eac27089716f88967806e63cb71d1abdf9fbf
abdessalam-aadel <abdessalam.aadel@gmail.com>
2020-01-10 19:39:06 +0000

    Add new user control (AddSubtitles.cs) :add subtitle to a video

---
+++ Conversion_Multimedia/AddSubtitles.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Conversion_Multimedia
+{
+    partial class AddSubtitles
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLoadSub = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxSubFilename = new System.Windows.Forms.TextBox();
+            this.txtBoxVideoFilename = new System.Windows.Forms.TextBox();
+            this.BtnLoadVideo = new System.Windows.Forms.Button();
+            this.BtnStartAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnLoadSub
+            // 
+            this.BtnLoadSub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLoadSub.Enabled = false;
+            this.BtnLoadSub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLoadSub.Location = new System.Drawing.Point(244, 93);
+            this.BtnLoadSub.Name = "BtnLoadSub";
+            this.BtnLoadSub.Size = new System.Drawing.Size(66, 24);
+            this.BtnLoadSub.TabIndex = 1;
+            this.BtnLoadSub.Text = "...";
+            this.BtnLoadSub.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnLoadSub.UseVisualStyleBackColor = true;
+            this.BtnLoadSub.Click += new System.EventHandler(this.BtnLoadSub_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(157, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Add Subtitles";
+            // 
+            // txtBoxSubFilename
+            // 
+            this.txtBoxSubFilename.Enabled = false;
+            this.txtBoxSubFilename.Location = new System.Drawing.Point(17, 95);
+            this.txtBoxSubFilename.Name = "txtBoxSubFilename";
+            this.txtBoxSubFilename.ReadOnly = true;
+            this.txtBoxSubFilename.Size = new System.Drawing.Size(212, 20);
+            this.txtBoxSubFilename.TabIndex = 15;
+            this.txtBoxSubFilename.Text = "Chose your subtitle ...";
+            // 
+            // txtBoxVideoFilename
+            // 
+            this.txtBoxVideoFilename.Location = new System.Drawing.Point(17, 64);
+            this.txtBoxVideoFilename.Name = "txtBoxVideoFilename";
+            this.txtBoxVideoFilename.ReadOnly = true;
+            this.txtBoxVideoFilename.Size = new System.Drawing.Size(212, 20);
+            this.txtBoxVideoFilename.TabIndex = 17;
+            this.txtBoxVideoFilename.Text = "Chose your video file ...";
+            // 
+            // BtnLoadVideo
+            // 
+            this.BtnLoadVideo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLoadVideo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLoadVideo.Location = new System.Drawing.Point(244, 62);
+            this.BtnLoadVideo.Name = "BtnLoadVideo";
+            this.BtnLoadVideo.Size = new System.Drawing.Size(66, 24);
+            this.BtnLoadVideo.TabIndex = 16;
+            this.BtnLoadVideo.Text = "...";
+            this.BtnLoadVideo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnLoadVideo.UseVisualStyleBackColor = true;
+            this.BtnLoadVideo.Click += new System.EventHandler(this.BtnLoadVideo_Click);
+            // 
+            // BtnStartAdd
+            // 
+            this.BtnStartAdd.BackColor = System.Drawing.Color.OrangeRed;
+            this.BtnStartAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnStartAdd.Enabled = false;
+            this.BtnStartAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnStartAdd.ForeColor = System.Drawing.Color.White;
+            this.BtnStartAdd.Location = new System.Drawing.Point(329, 62);
+            this.BtnStartAdd.Name = "BtnStartAdd";
+            this.BtnStartAdd.Size = new System.Drawing.Size(75, 55);
+            this.BtnStartAdd.TabIndex = 18;
+            this.BtnStartAdd.Text = "Start";
+            this.BtnStartAdd.UseVisualStyleBackColor = false;
+            this.BtnStartAdd.Click += new System.EventHandler(this.BtnStartAdd_Click);
+            // 
+            // AddSubtitles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BtnStartAdd);
+            this.Controls.Add(this.txtBoxVideoFilename);
+            this.Controls.Add(this.BtnLoadVideo);
+            this.Controls.Add(this.txtBoxSubFilename);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnLoadSub);
+            this.Name = "AddSubtitles";
+            this.Size = new System.Drawing.Size(420, 124);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnLoadSub;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxSubFilename;
+        private System.Windows.Forms.TextBox txtBoxVideoFilename;
+        private System.Windows.Forms.Button BtnLoadVideo;
+        private System.Windows.Forms.Button BtnStartAdd;
+    }
+}
---
+++ Conversion_Multimedia/AddSubtitles.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Conversion_Multimedia
+{
+    public partial class AddSubtitles : UserControl
+    {
+        public string videoName;
+        
+        public AddSubtitles() => InitializeComponent();
+
+        // Customize Open file dialog 
+        OpenFileDialog ofd = new OpenFileDialog
+        {
+            InitialDirectory = "C:/",
+            RestoreDirectory = true,
+            Title = "Chose your file"
+        };
+
+        // Handle event click Button Load video ...
+        private void BtnLoadVideo_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Videos files (*.mp4, *.mov, *.m4a, *.3gp, *.3g2, *.mj2) | *.mp4; *.mov; *.m4a; *.3gp; *.3g2; *.mj2";
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtBoxVideoFilename.Text = ofd.FileName;
+                txtBoxSubFilename.Enabled = true;
+                BtnLoadSub.Enabled = true;
+                videoName = Path.GetFileNameWithoutExtension(ofd.SafeFileName);
+            }
+        }
+
+        // Handle event click Button Load Subtitles ...
+        private void BtnLoadSub_Click(object sender, EventArgs e)
+        {
+            ofd.FileName = ""; // Clear open file dialog FileName
+            ofd.Filter = "Subtitles files (*.srt) | *.srt";
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtBoxSubFilename.Text = ofd.FileName;
+                BtnStartAdd.Enabled = true;
+                BtnLoadSub.Enabled = false;
+                BtnLoadVideo.Enabled = false;
+            }
+        }
+
+        // Handle event click Button Start Add Subtitles to video ...
+        private void BtnStartAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // change the cursor and disable button start
+                this.Cursor = Cursors.WaitCursor;
+                BtnStartAdd.Enabled = false;
+                // uses an instance of the Process class to start a process
+                using (Process process = new Process())
+                {
+                    process.StartInfo.UseShellExecute = false;
+                    // run the cmd process
+                    process.StartInfo.FileName = "cmd.exe";
+                    // Given that is started without a window
+                    process.StartInfo.CreateNoWindow = true;
+
+                    // uses the redirected input-output
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+
+                    // Start process
+                    process.Start();
+
+                    // Declare variable input and output of FFmpeg tools
+                    string inputVideo = txtBoxVideoFilename.Text;
+                    string inputSubtitle = txtBoxSubFilename.Text;
+
+                    string output = " output_" + videoName.Replace(" ", "_") + ".mp4";
+                    string ffmpeg;
+
+                    // Start Condition : if you have win32 or win64
+                    if (Environment.Is64BitOperatingSystem)
+                        ffmpeg = @"tools\x64\bin\ffmpeg.exe"; // path of FFmpeg tools for win32
+                    else
+                        ffmpeg = @"tools\x32\bin\ffmpeg.exe"; // path of FFmpeg tools for win64
+
+                    // Start Command line ...
+                    process.StandardInput.WriteLine(ffmpeg + " -i " + "\"" + inputVideo + "\"" 
+                        + " -vf subtitles=" + "\"" + inputSubtitle + "\"" 
+                        + output);
+                    
+                    // Flush & Close StandarInput
+                    process.StandardInput.Flush();
+                    process.StandardInput.Close();
+
+                    // Wait for Exit
+                    process.WaitForExit();
+
+                    // Close The Process
+                    process.Close();
+                }
+                ChangeToDefault();
+                MessageBox.Show("Your subtitles has been added successfully", "Success",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                ChangeToDefault();
+            }
+        }
+
+        // Methode Change to default
+        private void ChangeToDefault()
+        {
+            this.Cursor = DefaultCursor;
+            txtBoxVideoFilename.Text = "Chose your video file ...";
+            txtBoxSubFilename.Text = "Chose your subtitle ...";
+            txtBoxSubFilename.Enabled = false;
+            BtnLoadVideo.Enabled = true;
+            BtnLoadSub.Enabled = false;
+            BtnStartAdd.Enabled = false;
+            ofd.FileName = "";
+        }
+    }
+}
