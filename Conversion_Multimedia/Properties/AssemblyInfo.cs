2815252d7d461545fa67ca055655f5418cec1d93
abdessalam-aadel <abdessalam.aadel@gmail.com>
2020-01-10 19:28:39 +0000

    Update for version 2.0

--- Conversion_Multimedia/Properties/AssemblyInfo.cs
+++ Conversion_Multimedia/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Conversion_Multimedia")]
-[assembly: AssemblyDescription("A simple programme that allow you can convert all media")]
+[assembly: AssemblyDescription("Conversion Multimedia is a simple programme that allow you can convert all media")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("with FFmpeg tools")]
+[assembly: AssemblyCompany("FFmpeg tools")]
 [assembly: AssemblyProduct("Conversion_Multimedia")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
